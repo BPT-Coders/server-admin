971b1242147632402ce2d402b706c561ad543775
bgmir607605 <bgmir213@gmail.com>
2017-04-09 18:28:27 +0400

    Редактирование списка разрешённых ip #3

--- ajax/banSetNewMode.php
+++ ajax/banSetNewMode.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'Filer.php';
 
-$ip = $_POST["mode"];
+$mode = $_POST["mode"];
 $file = new Filer($_SERVER['DOCUMENT_ROOT'].'/data/banNewMode');
 $file->clear();
-$file->add($ip);
+$file->add($mode);
 
 ?>
